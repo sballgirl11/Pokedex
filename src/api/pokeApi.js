889bfd11883795109e205cff1e1efc43a0361b5b
sballgirl11 <brittney.postma@hotmail.com>
2020-05-19 09:50:28 -0400

    fixed fetch

--- src/api/pokeApi.js
+++ src/api/pokeApi.js
@@ -1,36 +1,24 @@
 export const getPokemon = async () => {
-  try {
-    const res = await fetch('https://pokeapi.com/api/v2/pokemon/?limit=150')
-    const data = await res.json()
-    return Promise.all(
-      data.map(async pokemon => {
-        const result = await fetchPokemon(pokemon)
-        return result
-      })
-    )
-  } catch (err) {
-    return console.log('error fetching pokemon: ', err)
-  }
-  // .then(allPokemon =>
-  //   allPokemon.results.forEach(pokemon => fetchPokemonData(pokemon))
-  // )
-  // .catch(error => error)
+	try {
+		const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+		const data = await res.json()
+		return Promise.all(
+			data.results.map(async pokemon => {
+				const result = await fetchPokemon(pokemon)
+				return result
+			})
+		)
+	} catch (err) {
+		return console.log('error fetching pokemon: ', err)
+	}
 }
 
 async function fetchPokemon(pokemon) {
-  try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-    )
-    return await response.json()
-  } catch (e) {
-    throw new Error(`fetching ${pokemon.name}'s details went wrong`)
-  }
+	try {
+		let url = pokemon.url
+		const response = await fetch(url)
+		return await response.json()
+	} catch (e) {
+		throw new Error(`fetching ${pokemon.name}'s details went wrong`)
+	}
 }
-
-// async function fetchPokemonData(pokemon) {
-//   let url = pokemon.url
-//   return await fetch(url)
-//     .then(res => res.json())
-//     .catch(error => error)
-// }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,29 +7,29 @@ import Pokemon from '../components/Pokemon/Pokemon'
 import './pageStyles.css'
 
 const HomePage = () => {
-  const dispatch = useDispatch()
-  const error = useSelector(state => state.pokemon.hasError)
-  const pending = useSelector(state => !state.pokemon.isLoaded)
-  const search = useSelector(state => state.search.search)
-  const pokemon = useSelector(state => state.pokemon.pokemon)
-  // const filteredPokemon = pokemon.filter(robot => {
-  //   return robot.name.toLowerCase().includes(search.toLowerCase())
-  // })
-  useEffect(() => {
-    dispatch(sendPokemon())
-  }, [dispatch])
-  return (
-    <Layout>
-      <SearchBox />
-      {pending ? (
-        <h1>Loading...</h1>
-      ) : error ? (
-        <h1>Uh oh! Something went wrong.</h1>
-      ) : (
-        <Pokemon />
-      )}
-    </Layout>
-  )
+	const dispatch = useDispatch()
+	const error = useSelector(state => state.pokemon.hasError)
+	const pending = useSelector(state => !state.pokemon.isLoaded)
+	const search = useSelector(state => state.search.search)
+	const pokemon = useSelector(state => state.pokemon.pokemon)
+	const filteredPokemon = pokemon.filter(robot => {
+		return robot.name.toLowerCase().includes(search.toLowerCase())
+	})
+	useEffect(() => {
+		dispatch(sendPokemon())
+	}, [dispatch])
+	return (
+		<Layout>
+			<SearchBox />
+			{pending ? (
+				<h1>Loading...</h1>
+			) : error ? (
+				<h1>Uh oh! Something went wrong.</h1>
+			) : (
+				<Pokemon pokemon={filteredPokemon} />
+			)}
+		</Layout>
+	)
 }
 
 export default HomePage
