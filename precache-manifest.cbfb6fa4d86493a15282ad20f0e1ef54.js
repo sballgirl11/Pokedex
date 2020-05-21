e428be45de6e745ac54073ac73610f223cb0acf1
Brittney <brittney.postma@hotmail.com>
2020-05-20 22:32:08 -0400

    Updates

---
+++ precache-manifest.cbfb6fa4d86493a15282ad20f0e1ef54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4fa1feaf87be7b1dd8b783ca168302",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "af4f30fe91d6074d0800",
+    "url": "/pokedex/static/css/main.377f651b.chunk.css"
+  },
+  {
+    "revision": "68557d9cb08a58dd85ef",
+    "url": "/pokedex/static/js/2.7aeb3814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.7aeb3814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4f30fe91d6074d0800",
+    "url": "/pokedex/static/js/main.7960addc.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/static/media/Pokemon Solid.36d26dbc.ttf"
+  }
+]);
